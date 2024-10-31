b393c452dbca7854f6bf110beeea86af594959ff
Kazi Fahad Mahin <151399467+mahinfah@users.noreply.github.com>
2024-10-31 20:40:26 +0600

    Update A. Sum of Round Numbers.cpp

--- A. Sum of Round Numbers.cpp
+++ A. Sum of Round Numbers.cpp
@@ -1,35 +1,68 @@
+
+//https://codeforces.com/problemset/problem/1352/A
+
 #include<iostream>
+#include<string>
+#include <cmath>
+
+
 using namespace std;
 int main()
 {
-   int n , m;
-   cin>>n;
-   //cin>>m;
 
-   int count = 0 ;
 
-    for(int i = 0; i<n ; i++)
+    int b;
+    cin>>b;
+
+
+    for(int i=0;i<b;i++)
     {
-   int a ;
-   cin>>a;
-       if(a<10&&a>0)
-       {
-           cout<<"1"<<endl;
-           cout<<a<<endl;
-       }
-       if(a%10==0)
-          {
+        int l;
 
-           cout<<"1"<<endl;
-           cout<<a<<endl;
+        string a;
 
-          }
-          if(a%10!=0)
-          {
+        cin>>a;
+        int count=0;
 
-          }
+       int len=a.length();
+
+        for(int i =0;i<len;i++)
+        {
+            if(a[i]!='0')
+            {
+                count++;
+            }
+
+
+        }
+
+         cout<<count<<endl;
+
+
+          for(int i=0;i<len-1;i++)
+        {
+
+         if(a[i]!='0')
+           {
+                   int p=i+1;
+                   int pp=len-p;
+                   int number = 10;
+                   int square=1;
+                   square = pow(10, pp);
+                //   cout<<square<<"S :      ";
+                   char c = a[i];
+                   int convert = c - '0';
+                   int result=1;
+                   result= convert*square;
+                          cout<<result <<" ";
+           }
+        }
+        if(a[len-1]!='0')
+            cout<<a[len-1];
+        cout<<endl;
 
 
     }
+
 
 }
