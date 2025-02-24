aa4e9d10ad205004231a37f0b789c8cd9ab1663e
Kazi Fahad Mahin <151399467+mahinfah@users.noreply.github.com>
2025-02-24 00:28:10 -0800

    Add files via upload

---
+++ A. Game with Integers.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int n;
+     cin>>n;
+     for(int i=1;i<=n;i++)
+     {
+
+
+         int a;
+         cin>>a;
+         if(a%3==0)
+         {
+             cout<<"Second"<<endl;
+         }
+         else
+         {
+             cout<<"First"<<endl;
+         }
+
+     }
+
+
+}
