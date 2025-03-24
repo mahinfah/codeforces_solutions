5f29b3ca002e93eaef93ee3167608fe6d12b7078
Kazi Fahad Mahin <151399467+mahinfah@users.noreply.github.com>
2025-03-24 23:22:04 +0600

    Create A. Keyboard.cpp

---
+++ A. Keyboard.cpp
@@ -0,0 +1,351 @@
+//https://codeforces.com/contest/474/problem/A
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string a,b;
+
+    cin>> b;
+
+    if(b=="L")
+    { cin>>a;
+        int e=a.length();
+      for(int i=0 ;i<e;i++)
+      {
+          if(a[i]=='a')
+          {
+              cout<<'s';
+          }
+
+
+         else if(a[i]=='s')
+          {
+              cout<<'d';
+          }
+
+         else if(a[i]=='d')
+          {
+              cout<<'f';
+          }
+
+
+         else if(a[i]=='f')
+          {
+              cout<<'g';
+          }
+
+
+          else if(a[i]=='g')
+          {
+              cout<<'h';
+          }
+
+
+          else if(a[i]=='h')
+          {
+              cout<<'j';
+          }
+
+
+          else if(a[i]=='j')
+          {
+              cout<<'k';
+          }
+
+
+          else if(a[i]=='k')
+          {
+              cout<<'l';
+          }
+
+
+          else if(a[i]=='l')
+          {
+              cout<<';';
+          }
+
+
+          else if(a[i]=='z')
+          {
+              cout<<'x';
+          }
+
+
+          else if(a[i]=='x')
+          {
+              cout<<'c';
+          }
+
+
+          else if(a[i]=='c')
+          {
+              cout<<'v';
+          }
+
+
+          else if(a[i]=='v')
+          {
+              cout<<'b';
+          }
+
+
+          else if(a[i]=='b')
+          {
+              cout<<'n';
+          }
+
+
+
+          else if(a[i]=='n')
+          {
+              cout<<'m';
+          }
+
+
+          else if(a[i]=='m')
+          {
+              cout<<',';
+          }
+
+          else if(a[i]==',')
+          {
+              cout<<'.';
+          }
+
+          else if(a[i]=='.')
+          {
+              cout<<'/';
+          }
+
+          else if(a[i]=='q')
+          {
+              cout<<'w';
+          }
+
+          else if(a[i]=='w')
+          {
+              cout<<'e';
+          }
+
+          else if(a[i]=='e')
+          {
+              cout<<'r';
+          }
+
+          else if(a[i]=='r')
+          {
+              cout<<'t';
+          }
+
+          else if(a[i]=='t')
+          {
+              cout<<'y';
+          }
+
+         else  if(a[i]=='y')
+          {
+              cout<<'u';
+          }
+
+         else  if(a[i]=='i')
+          {
+              cout<<'o';
+          }
+
+         else  if(a[i]=='u')
+          {
+              cout<<'i';
+          }
+
+         else  if(a[i]=='o')
+          {
+              cout<<'p';
+          }
+
+         else  if(a[i]=='p')
+          {
+              cout<<'[';
+          }
+
+
+
+
+
+
+
+
+
+
+      }
+
+
+    }
+    else if(b=="R")
+    {
+      cin>>a;
+        int e=a.length();
+      for(int i=0 ;i<e;i++)
+      {
+          if(a[i]=='s')
+          {
+              cout<<'a';
+          }
+
+
+         else if(a[i]=='d')
+          {
+              cout<<'s';
+          }
+
+         else if(a[i]=='f')
+          {
+              cout<<'d';
+          }
+
+
+         else if(a[i]=='g')
+          {
+              cout<<'f';
+          }
+
+
+          else if(a[i]=='h')
+          {
+              cout<<'g';
+          }
+
+
+          else if(a[i]=='j')
+          {
+              cout<<'h';
+          }
+
+
+          else if(a[i]=='k')
+          {
+              cout<<'j';
+          }
+
+
+          else if(a[i]=='l')
+          {
+              cout<<'k';
+          }
+
+
+          else if(a[i]==';')
+          {
+              cout<<'l';
+          }
+
+
+          else if(a[i]=='x')
+          {
+              cout<<'z';
+          }
+
+
+          else if(a[i]=='c')
+          {
+              cout<<'x';
+          }
+
+
+          else if(a[i]=='v')
+          {
+              cout<<'c';
+          }
+
+
+          else if(a[i]=='b')
+          {
+              cout<<'v';
+          }
+
+
+          else if(a[i]=='n')
+          {
+              cout<<'b';
+          }
+
+
+
+          else if(a[i]=='m')
+          {
+              cout<<'n';
+          }
+
+
+          else if(a[i]==',')
+          {
+              cout<<'m';
+          }
+
+          else if(a[i]==',')
+          {
+              cout<<'.';
+          }
+
+          else if(a[i]=='/')
+          {
+              cout<<'.';
+          }
+
+          else if(a[i]=='w')
+          {
+              cout<<'q';
+          }
+
+          else if(a[i]=='e')
+          {
+              cout<<'w';
+          }
+
+          else if(a[i]=='r')
+          {
+              cout<<'e';
+          }
+
+          else if(a[i]=='t')
+          {
+              cout<<'r';
+          }
+
+          else if(a[i]=='y')
+          {
+              cout<<'t';
+          }
+
+         else  if(a[i]=='u')
+          {
+              cout<<'y';
+          }
+
+         else  if(a[i]=='i')
+          {
+              cout<<'u';
+          }
+
+         else  if(a[i]=='o')
+          {
+              cout<<'i';
+          }
+
+         else  if(a[i]=='p')
+          {
+              cout<<'o';
+          }
+
+         else  if(a[i]=='[')
+          {
+              cout<<'p';
+          }
+else  if(a[i]=='.')
+          {
+              cout<<',';
+          }
+
+    }
+
+}
+}
